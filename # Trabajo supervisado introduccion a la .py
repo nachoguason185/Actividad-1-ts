dc7b937127c66ab77a5346eb8111523830a9af18
nachoguason185 <144157992+nachoguason185@users.noreply.github.com>
2023-09-07 09:35:33 -0600

    Add files via upload

---
+++ # Trabajo supervisado introduccion a la .py
@@ -0,0 +1,22 @@
+# Trabajo supervisado introduccion a la programacion seccion: 17
+# Fecha de creacion de programa: 7/09/2023
+# Diego Ignacio Sánchez López
+
+# Objetivo: Crear un programa que solicite los datos del usuario, los almacene en variables y los muestre en la pantalla. 
+# Entrada: Nombre, Edad, Carrera, Carne
+# Procesos principales
+print("Mi segundo programa")
+sNombre=input("Ingrese su nombre")
+print("Nombre:"+ sNombre)
+
+sedad=input("Ingrese su edad")
+print("edad:"+ sedad)
+
+scarrera=input("Ingrese carrera")
+print("carrera:"+ scarrera)
+
+scarne=input("Ingrese su carne")
+print("Nombre:"+ scarne)
+
+# Salida
+print("Soy" + sNombre + " tengo " + sedad + " años y estudio la carrera de " +scarrera+ " Mi numero de carne es: "+scarne)
